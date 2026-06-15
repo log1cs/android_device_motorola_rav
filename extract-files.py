442bade83a9d44693c4ebeb355de64b4e78c470e
Tuan Anh <tuan73176@gmail.com>
2026-06-15 02:05:03 +0000

    rav: Initial vendor blobs
    Change-Id: Ie9a44e8c56fbd963bf5e05f20acd1ba6d8c1d924

--- extract-files.py
+++ extract-files.py
@@ -4,6 +4,14 @@
 # SPDX-License-Identifier: Apache-2.0
 #
 
+from extract_utils.fixups_blob import (
+    blob_fixup,
+    blob_fixups_user_type,
+)
+from extract_utils.fixups_lib import (
+    lib_fixups,
+    lib_fixups_user_type,
+)
 from extract_utils.main import (
     ExtractUtils,
     ExtractUtilsModule,
@@ -13,10 +21,20 @@ namespace_imports = [
     'vendor/motorola/sm6125-common',
 ]
 
+lib_fixups: lib_fixups_user_type = {
+    **lib_fixups,
+}
+
+blob_fixups: blob_fixups_user_type = {
+    # Failing here is ok.
+}  # fmt: skip
+
 module = ExtractUtilsModule(
     'rav',
     'motorola',
     namespace_imports=namespace_imports,
+    blob_fixups=blob_fixups,
+    lib_fixups=lib_fixups,
 )
 
 if __name__ == '__main__':
